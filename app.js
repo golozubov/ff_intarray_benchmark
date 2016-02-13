61e7b33edaab0b78ba8973118ed3f98a9514c63d
artem_golozubov <golozubov@gmail.com>
2016-02-13 18:31:39 +0300

    Add module faker and use it in user creation

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 import bluebird from 'bluebird'
+import {default as faker} from 'faker'
 import {knex} from './db.js'
 import {DbCleaner} from './dbCleaner'
+
 
 global.Promise = bluebird
 global.Promise.onPossiblyUnhandledRejection((e) => { throw e; });
 
 
 async function app() {
-  let ids = await knex('users').returning('id').insert({
-    username:   "test123213",
-    screenname: "33",
-    email:      "text1234",
-    pw_hash:    ""
+  let username      = faker.internet.userName().replace(/_/g, '').replace(/\./g, '')
+  let screenName    = faker.name.findName()
+  let email         = faker.internet.email()
+  let passwordHash  = faker.internet.password()
+  let id = await knex('users').returning('id').insert({
+    username:   username,
+    screenname: screenName,
+    email:      email,
+    pw_hash:    passwordHash
   })
 
-  console.log(ids)
+  console.log(id)
 
   let res = await knex.select('id').from('users')
   console.log(res)
